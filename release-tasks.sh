ca7ef3aa81cb459459ecfe26b66f5eab16b9a8a4
Ryan Schaffer <ryan@tacticalcodemonkeys.com>
2018-09-02 17:16:03 -0400

    Include all the config goodies

--- release-tasks.sh
+++ release-tasks.sh
@@ -2,4 +2,4 @@
 python manage_db.py db init || true
 python manage_db.py db upgrade
 python ensure_admin.py
-python zcrm_config.py https://parseleads.herokuapp.com/oauth2callback
+python zcrm_config.py $HEROKU_APP_NAME/oauth2callback
